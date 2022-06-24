60d26293cf65d97f3b77dcbfcd9dc9037756dac9
santhitak <santhita.sk@gmail.com>
2022-06-24 07:09:23 +0700

    Style: Responsive display for any device

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,5 @@
-import Head from 'next/head';
-import { useEffect, useState } from 'react';
+import Head from "next/head";
+import { useEffect, useState } from "react";
 import {
   Box,
   Container,
@@ -10,59 +10,82 @@ import {
   Kbd,
   Button,
   StatHelpText,
-} from '@chakra-ui/react'
+  SimpleGrid,
+} from "@chakra-ui/react";
 
 export default function Home() {
   const [price, setPrice] = useState(0);
   const [unit, setUnit] = useState("BTC");
 
   const units = {
-    "BTC": [1, 2],
-    "mBTC": [1e-3, 2],
-    "µBTC": [1e-6, 4],
-    "Sats": [1e-8, 6],
-  }
+    BTC: [1, 2],
+    mBTC: [1e-3, 2],
+    µBTC: [1e-6, 4],
+    Sats: [1e-8, 6],
+  };
 
   useEffect(() => {
-    const socket = new WebSocket("wss://stream.binance.com:9443/ws/btcusdt@trade");
+    const socket = new WebSocket(
+      "wss://stream.binance.com:9443/ws/btcusdt@trade"
+    );
     socket.onmessage = (e) => {
-      setPrice(JSON.parse(e.data).p || price)
-    }
+      setPrice(JSON.parse(e.data).p || price);
+    };
 
-    document.addEventListener("keydown", e => {
+    document.addEventListener("keydown", (e) => {
       Object.keys(units).map((val, index) => {
-        if (e.key === String(index+1)) setUnit(val);
-      })
-    })
-  }, [])
+        if (e.key === String(index + 1)) setUnit(val);
+      });
+    });
+  }, []);
 
   return (
     <>
       <Head>
         <title>Bitcoin Price</title>
       </Head>
-      <Box backgroundColor="#f0f0f0">
-        <Container maxWidth="720px" height="calc(100vh)">
-          <Flex height="100%" direction="column" justify="center" align="center" gap="8">
-            <Box borderRadius="xl" padding="6" backgroundColor="#1e2227" color="white" width="360px" boxShadow="lg">
+      <Box>
+        <Container height="calc(100vh)">
+          <Flex
+            gap="8"
+            height="100%"
+            align="center"
+            justify="center"
+            direction="column"
+          >
+            <Box
+              padding="6"
+              color="white"
+              width="360px"
+              boxShadow="lg"
+              borderRadius="xl"
+              backgroundColor="#1e2227"
+            >
               <Stat>
                 <StatLabel>Binance BTC-USDT</StatLabel>
                 <StatNumber>
-                  { (price * units[unit][0]).toFixed(units[unit][1]) }$
+                  {(price * units[unit][0]).toFixed(units[unit][1])}$
                 </StatNumber>
-                <StatHelpText>Per { unit }</StatHelpText>
+                <StatHelpText>Per {unit}</StatHelpText>
               </Stat>
             </Box>
-            <Flex gap="8" justify="center">
-              {
-                Object.keys(units).map((elem, index) => (
-                  <Button leftIcon={<Kbd borderColor="teal">{ index + 1 }</Kbd>} colorScheme='teal' borderRadius="xl" variant={unit === elem ? 'outline' : 'ghost'} key={index} onClick={() => setUnit(elem)}>{ elem }</Button>
-                ))
-              }
-            </Flex>
+            <SimpleGrid columns={[2, null, 4]} gap={8}>
+              {Object.keys(units).map((elem, index) => (
+                <Button
+                  leftIcon={<Kbd borderColor="teal">{index + 1}</Kbd>}
+                  colorScheme="teal"
+                  borderRadius="xl"
+                  variant={unit === elem ? "outline" : "ghost"}
+                  key={index}
+                  onClick={() => setUnit(elem)}
+                >
+                  {elem}
+                </Button>
+              ))}
+            </SimpleGrid>
           </Flex>
         </Container>
       </Box>
     </>
-  )
+  );
 }
